d92d867dfa4b2b6d83e194b4255f0be8b781edb9
The-Gentleman <zraja4402@gmail.com>
2021-05-09 16:32:05 -0400

    added serializer for user#index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < ApplicationController
     def index
         @users = User.all 
-        render json: @users
+        # render json: @users
+        user_json = UserSerializer.new(@users).serialized_json
+        render json: user_json
     end 
 end
