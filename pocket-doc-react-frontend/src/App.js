99f0191afa2608ba837d231c26a73ab63c3d34fe
The-Gentleman <zraja4402@gmail.com>
2021-05-10 17:31:28 -0400

    created PatientList component

--- pocket-doc-react-frontend/src/App.js
+++ pocket-doc-react-frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.js'
+import PatientList from './components/PatientList.js'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PatientList />
     </div>
   );
 }
