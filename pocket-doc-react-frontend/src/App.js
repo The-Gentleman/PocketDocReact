90d79ce85820a7463dbe035c4cce556268240d82
The-Gentleman <zraja4402@gmail.com>
2021-05-10 17:27:44 -0400

    added header component

--- pocket-doc-react-frontend/src/App.js
+++ pocket-doc-react-frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js'
 
 function App() {
   return (
     <div className="App">
-      Hi!
+      <Header />
     </div>
   );
 }
