c1bf28104464c9eb4aaeccf205b0fed8b258f32e
The-Gentleman <zraja4402@gmail.com>
2021-05-10 11:09:03 -0400

    modified helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::API
-    helpers do 
-        def current_user
-            User.first
-        end 
+    def current_user
+        User.first
     end 
 end
