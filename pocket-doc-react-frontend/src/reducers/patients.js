0faf2ccf02eaad214af96484c2687158be415905
The-Gentleman <zraja4402@gmail.com>
2021-05-12 11:28:40 -0400

    fixed case name

--- pocket-doc-react-frontend/src/reducers/patients.js
+++ pocket-doc-react-frontend/src/reducers/patients.js
@@ -1,7 +1,7 @@
 export default (state = [], action) => {
     switch (action.type) {
-        case "PATIENT_NAME":
-            return action.patient;
+        case "FETCH_PATIENTS":
+            return action.payload;
         default:
             return state;
     }
