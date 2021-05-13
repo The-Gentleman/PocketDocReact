c780e90d151077906563f65ccd14ba6af9b02700
The-Gentleman <zraja4402@gmail.com>
2021-05-13 15:32:47 -0400

    iterated through dispatched object to render names to dom--WOOO

--- pocket-doc-react-frontend/src/components/PatientList.js
+++ pocket-doc-react-frontend/src/components/PatientList.js
@@ -4,17 +4,19 @@ import { fetchPatients  } from '../actions/patients';
 
 class PatientList extends Component {
 
-    constructor(props){
-        super(props);
-    }
-
     componentDidMount() {
+        this.props.dispatchFetchPatients()
     }
-
+    // this.props.patients
     render() {
         return (
             <div className="patient-names">
                 <h3>Your patients:</h3>
+                {
+                    this.props.patients.map(patient =>{
+                        return <ol>{patient.name}</ol>
+                    })
+                }
             </div>
         )
     }
@@ -24,7 +26,7 @@ class PatientList extends Component {
 const mapStateToProps = (state) => {
         return {patients: state.patients}
 }
-// to give a function that updates the store
+// mapDispatchToProps: to give a function that updates the store
 // dispatch is reduxs setState
 const mapDispatchToProps = dispatch => {
         return {
@@ -35,8 +37,3 @@ const mapDispatchToProps = dispatch => {
 
 export default connect(mapStateToProps, mapDispatchToProps)(PatientList);
 
-// QUESTIONS FOR STUDY GROUP
-// 1-  Everytime I want to change a patients attribute,
-//  I'd have to do this process all over again?
-// 2- When I add to my reducer, I get that cases differ,
-// but what would I return?
