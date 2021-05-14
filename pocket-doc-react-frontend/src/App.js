eb166cb4ebb5f7dd9f6aa04c97f34268f02e774f
The-Gentleman <zraja4402@gmail.com>
2021-05-14 00:15:26 -0400

    added links

--- pocket-doc-react-frontend/src/App.js
+++ pocket-doc-react-frontend/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Header from './components/Header.js'
 import PatientList from './components/PatientList.js'
-import { Switch, Route } from 'react-router-dom' 
+import { Switch, Route, Link } from 'react-router-dom' 
 
 function App() {
   return (
    
       <div className="App">
+        <div>
+        <Link to="/patients">Patients</Link> | <Link to="/Home">Home</Link>
+        </div>
         <Switch>
+          <Route exact path="/" component={Header} />
           <Route exact path="/patients" component={PatientList} />
           <Header />
           <PatientList patientName="" />
