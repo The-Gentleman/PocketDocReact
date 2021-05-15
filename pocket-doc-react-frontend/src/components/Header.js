67002d8ec9dd902e82db8473457d5e80f5a3c2f3
The-Gentleman <zraja4402@gmail.com>
2021-05-15 10:13:53 -0400

    added text

--- pocket-doc-react-frontend/src/components/Header.js
+++ pocket-doc-react-frontend/src/components/Header.js
@@ -4,6 +4,10 @@ export default function Header() {
     return (
         <div>
             <h1>Welcome to PocketDoc!</h1>
+            <strong>One place where you can care for you patients without having to smell their stank breath.</strong>
+            <br></br>
+            <br></br>
+            <label>To view your patients, just use the link above.</label>
         </div>
     )
 }
