7c468624b337ad4f6698670d2e9f9a9d10d5920b
The-Gentleman <zraja4402@gmail.com>
2021-05-17 13:08:37 -0400

    added dynamic link to routes

--- pocket-doc-react-frontend/src/App.js
+++ pocket-doc-react-frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header.js'
 import PatientList from './components/PatientList.js'
+import PatientCard from './components/PatientCard.js'
 import NoRoute from './components/NoRoute.js'
 import { Switch, Route, Link } from 'react-router-dom' 
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/home" component={Header} />
           <Route exact path="/patients" component={PatientList} />
+          <Route exact path="/patients/:id" component={PatientCard} />
           <Route component={NoRoute}/>
           <Header />
           <PatientList patientName="" />
