f10e1b084cf08c9eaaa5dff998380eababcd6fe8
The-Gentleman <zraja4402@gmail.com>
2021-05-17 13:10:06 -0400

    created new component

---
+++ pocket-doc-react-frontend/src/components/PatientCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PatientCard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Patient info here</h1>
+            </div>
+        )
+    }
+}
