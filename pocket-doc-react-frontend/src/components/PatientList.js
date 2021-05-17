ac2713b01bfe545ebd3c393b1b31b884c8007cc2
The-Gentleman <zraja4402@gmail.com>
2021-05-17 13:09:27 -0400

    added link to to render function

--- pocket-doc-react-frontend/src/components/PatientList.js
+++ pocket-doc-react-frontend/src/components/PatientList.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchPatients  } from '../actions/patients';
+import { fetchPatients  } from '../actions/patients'
+import { Link } from 'react-router-dom'
 
 class PatientList extends Component {
 
     componentDidMount() {
         this.props.dispatchFetchPatients()
     }
-    
     // now I need to make these names:
     //  1) Clikable links --> Gonna need a click listener
     //  2) Routed somewhere
@@ -15,7 +15,11 @@ class PatientList extends Component {
         return (
             <div className="patient-names">
                 <h3>Your patients:</h3>
-                {this.props.patients.map(patient => {return <ol>{patient.name}</ol>})}
+                {this.props.patients.map(patient => {
+                    return <ol>
+                        <Link to={`patients/${patient.id}`}>{patient.name}</Link>
+                        </ol>
+                    })}
             </div>
         )
     }
@@ -31,7 +35,7 @@ const mapDispatchToProps = dispatch => {
         return {
             dispatchFetchPatients: () => dispatch(fetchPatients())
         }
-};
+}
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(PatientList);
