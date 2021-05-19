dd465dd05bf7a148900893ddd8ea70a3d272ed6d
The-Gentleman <zraja4402@gmail.com>
2021-05-18 22:21:27 -0400

    created container directory

---
+++ pocket-doc-react-frontend/src/container/PatientCardContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PatientCard from '../components/PatientCard.js'
+
+export default class PatientCardContainer extends Component {
+    render() {
+        return (
+            <div>
+                Poop
+            </div>
+        )
+    }
+}
+
+
+// Mapstatetoprops
+// Mapdispatchtoprops
