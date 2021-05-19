3d57f549b4f207e33183980c94d83b36852ba149
The-Gentleman <zraja4402@gmail.com>
2021-05-18 22:20:08 -0400

    made header message more appropriate

--- pocket-doc-react-frontend/src/components/Header.js
+++ pocket-doc-react-frontend/src/components/Header.js
@@ -4,8 +4,6 @@ export default function Header() {
     return (
         <div>
             <h1>Welcome to PocketDoc!</h1>
-            <strong>One place where you can care for you patients without having to smell their stank breath.</strong>
-            <br></br>
             <br></br>
             <label>To view your patients, just use the link above.</label>
         </div>
