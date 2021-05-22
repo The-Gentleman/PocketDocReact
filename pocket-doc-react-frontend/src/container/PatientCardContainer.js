8c8734d7ca5e23206c26862839a59012449203df
The-Gentleman <zraja4402@gmail.com>
2021-05-22 00:40:04 -0400

    rendered patient info on screen. need to figure out why i cant refresh

--- pocket-doc-react-frontend/src/container/PatientCardContainer.js
+++ pocket-doc-react-frontend/src/container/PatientCardContainer.js
@@ -12,14 +12,27 @@ import { fetchPatients  } from '../actions/patients'
      render() {
         return (
             <div>
-                -----------------------------------------
+               <h2>Patient Name:</h2>{this.props.patientName}
+               <h2>Patient Symptoms:</h2>{this.props.patientSymptoms}
+               <h2>Maaaan... i dont even know:</h2>{<PatientCard />}
             </div>
         )
     }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    debugger
+    return { 
+        patientName: state.patients.find(patient => {
+        if (ownProps.match.params.id == patient.id) {
+            return patient  
+           }
+       }).name,
+       patientSymptoms: state.patients.find(patient => {
+        if (ownProps.match.params.id == patient.id) {
+            return patient 
+           }
+       }).symptoms
+    }
 }
 
 const mapDispatchToProps = dispatch => {
@@ -29,16 +42,9 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PatientCardContainer);
-
-// put redux logic in container component
-    // if no patients in store, props patients = undefined
-    // if no patient dispatchFetchPatient
-// pass prop into PatientCard
-
 // ***dispatchFetchPatients !== dispatch(fetchPatients())
 
 
-// match prop => patientID
 
 
 
