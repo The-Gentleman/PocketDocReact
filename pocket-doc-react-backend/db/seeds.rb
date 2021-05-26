b0528d895b6abb4fc97a05c973597c84d0673119
The-Gentleman <zraja4402@gmail.com>
2021-05-26 13:14:44 -0400

    reseeded db

--- pocket-doc-react-backend/db/seeds.rb
+++ pocket-doc-react-backend/db/seeds.rb
@@ -1,5 +1,5 @@
 phil = User.create(name: "Dr. Phil")
 
-patient_one = Patient.create(name: "Peter", symptoms: "Swelling around hand, increased eyesight, sudden increase in strength", diagnosis: "Spider-Man", home_exercise_plan: "Get in touch with Tony Stark and join the Avengers.", user_id: phil.id)
-patient_two = Patient.create(name: "Thanos", symptoms: "Charisma, inclination towards violence, twisted notion of virtue that manifests itself in killing half of all life.", diagnosis: "Total psychopath", home_exercise_plan: "Seek therapy.", user_id: phil.id)
-patient_three = Patient.create(name: "Bruce", symptoms: "Violent temper, brilliant scientist." , diagnosis: "You're the hulk.", home_exercise_plan: "Seek anger management and do some yoga.", user_id: phil.id)
+patient_one = Patient.create(name: "Peter", symptoms: "Swelling around hand, increased eyesight, sudden increase in strength", diagnosis: "", home_exercise_plan: "", user_id: phil.id)
+patient_two = Patient.create(name: "Thanos", symptoms: "Charisma, inclination towards violence, twisted notion of virtue that manifests itself in killing half of all life.", diagnosis: "", home_exercise_plan: "", user_id: phil.id)
+patient_three = Patient.create(name: "Bruce", symptoms: "Violent temper, brilliant scientist." , diagnosis: "", home_exercise_plan: "", user_id: phil.id)
