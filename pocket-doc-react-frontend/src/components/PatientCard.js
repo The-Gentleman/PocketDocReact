5764ce71ed22cf05e371e4ad4a6ce3432e2edcf0
The-Gentleman <zraja4402@gmail.com>
2021-05-27 20:45:05 -0400

    added marker thingy denoting where fields would go

--- pocket-doc-react-frontend/src/components/PatientCard.js
+++ pocket-doc-react-frontend/src/components/PatientCard.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 export default function PatientCard(props) {
+    // debugger
     return (
         <div className="PatientCard">
             <h2>Patient Name:</h2>{props.patient.name}
             <h2>Patient Symptoms:</h2>{props.patient.symptoms}
+            <br></br>
+            <br></br>
+            ------------FIELDS FOR DIAGNOSIS AND HOME EXERCISE PLAN BELOW------------------
         </div>
     )
 }
+
