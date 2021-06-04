d62eee3d42efaf01cb313bf5409260bfdb6a58a5
The-Gentleman <zraja4402@gmail.com>
2021-06-03 21:56:38 -0400

    rendering DiagnosisFormContainer

--- pocket-doc-react-frontend/src/components/PatientCard.js
+++ pocket-doc-react-frontend/src/components/PatientCard.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-
+import DiagnosisFormContainer from '../container/DiagnosisFormContainer'
 export default function PatientCard(props) {
-    // debugger
+
     return (
         <div className="PatientCard">
             <h2>Patient Name:</h2>{props.patient.name}
             <h2>Patient Symptoms:</h2>{props.patient.symptoms}
             <br></br>
             <br></br>
-            ------------FIELDS FOR DIAGNOSIS AND HOME EXERCISE PLAN BELOW------------------
+            <DiagnosisFormContainer />
         </div>
     )
 }
