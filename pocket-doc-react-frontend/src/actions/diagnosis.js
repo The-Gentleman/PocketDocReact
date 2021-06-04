c34c9e2ed5f5832db332b2b3c8d858e4767521ba
The-Gentleman <zraja4402@gmail.com>
2021-06-03 21:56:01 -0400

    modified this somehow

--- pocket-doc-react-frontend/src/actions/diagnosis.js
+++ pocket-doc-react-frontend/src/actions/diagnosis.js
@@ -1,4 +1,4 @@
-export const dispatchAddDiagnosis = (patient) => {
+export const addDiagnosis = (patient) => {
     return dispatch => {
         fetch(`http://localhost:3000/patients/${patient.id}`, {
             method: 'PATCH',
@@ -6,17 +6,13 @@ export const dispatchAddDiagnosis = (patient) => {
                 'Content-Type': 'application/json',
                 'Accept' : 'application/json'
               },
-            body: JSON.stringify(patient)
+            body: JSON.stringify({patient})
         })
         .then(response => response.json())
         .then(patientElement => {
+            return patientElement.data.attributes,
             dispatch({type: "ADD_DIAGNOSIS", payload: patientElement})
         })
     }
 }
 
-// Updating patient within this function
-    // fetch request to route that has patient ID
-    // patient diagnosis in body
-    // pass in arguement(s) and use that as the body
-    // payload represents the patient i just diagnosed
