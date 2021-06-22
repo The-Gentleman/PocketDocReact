21b5d63155057e607f9319f84e2b9a7ffb9bca5c
The-Gentleman <zraja4402@gmail.com>
2021-06-22 05:14:34 -0400

    added snark to noroute message

--- pocket-doc-react-frontend/src/components/NoRoute.js
+++ pocket-doc-react-frontend/src/components/NoRoute.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NoRoute() {
     return (
         <div className="NoRoute">
-            <h3>That route does not exist.</h3>
+            <h3>That route does not exist. Turn back at once!</h3>
         </div>
     )
 }
