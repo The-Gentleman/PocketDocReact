930894c58dfc9fc6f40f0ebdf4c7b0ad0ffc7fd1
The-Gentleman <zraja4402@gmail.com>
2021-07-03 12:19:37 -0400

    created component and added text to test

---
+++ pocket-doc-react-frontend/src/components/LikeButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class LikeButton extends Component {
+    
+    render() {
+        return (
+            <p>sup?</p>
+        )
+    }
+}
+
+export default LikeButton
