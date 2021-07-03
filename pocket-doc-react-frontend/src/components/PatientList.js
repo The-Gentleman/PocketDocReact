073e6d51679d98a5b9da27bc5e788aac7436a75e
The-Gentleman <zraja4402@gmail.com>
2021-07-03 11:58:29 -0400

    added notes for assessment assignment

--- pocket-doc-react-frontend/src/components/PatientList.js
+++ pocket-doc-react-frontend/src/components/PatientList.js
@@ -13,7 +13,9 @@ class PatientList extends Component {
         return (
             <div className="patient-names">
                 <h3>Your patients:</h3>
-                {this.props.patients.map(patient => {return <ol><Link to={`patients/${patient.id}`}>{patient.name}</Link></ol>})}
+                {this.props.patients.map(patient => 
+                    {return <ol><Link to={`patients/${patient.id}`}>{patient.name}</Link></ol>})
+                }
             </div>
         )
     }
@@ -31,4 +33,15 @@ const mapDispatchToProps = dispatch => {
         }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(PatientList);
+/*
+Aim: To create a like button next to the names of each patient with a numerical representation of how many times the button was pressed.
+What I would need:
+- Create a new component 
+    -within this new component, I need to track the patient and add local state.
+    -within local state, I would need to have a counter attribute that increased everytime the like button was pressed
+- Create an onClick handler
+    - Within this onClick handler, I need a counter to track event?(amount of times the button is clicked)
+- Call onChange next to the button within the return.
+*/
+
 
