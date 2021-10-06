b86f94705be8a85630fca893e82dbe0572156100
The-Gentleman <zraja4402@gmail.com>
2021-10-06 02:44:32 -0400

    cleaned up component

--- pocket-doc-react-frontend/src/components/PatientList.js
+++ pocket-doc-react-frontend/src/components/PatientList.js
@@ -7,46 +7,24 @@ import { Link } from 'react-router-dom'
 
 class PatientList extends Component {
     state = {
-        patients: this.props.patients,
-        query: ''
+        patients: this.props.patients
     }
 
     componentDidMount() {
         this.props.dispatchFetchPatients()
     }
 
-    componentDidUpdate(prevProps){
-        if (this.props.patients !== prevProps.patients) {
-            this.setState({
-                patients: this.props.patients
-            })
-        }
-    }
-
-    updateSearch = () => {
-        this.setState({
-            patients: this.props.patients.filter((patient) => patient.name.includes(this.state.query))
-        })
-    }
-
-    handleChange = (event) => {
-        event.preventDefault()
-        this.setState({
-            query: event.target.value
-        }, this.updateSearch)
+    handleClick = (event) => {
+        event.preventDefault();
+        let patientsObj = this.props.patients 
+        // patientsObj.sort((a,b) => a.name.localeCompare(b.name))
     }
 
     render() {
         return (
             <div className="patient-names">
 
-                <form>
-                    Search:
-                    <input type="text" value={this.state.query}onChange={this.handleChange}></input>
-                </form>
-
-
-                <h3>Your patients:</h3>
+            <h3>Your patients:</h3>
                 {this.props.patients.map(patient => 
                     {return <ol><Link to={`patients/${patient.id}`}>{patient.name}</Link><LikeButton /></ol>})
                 }
