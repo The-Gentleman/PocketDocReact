679222dcb3f81805569251d679b4e6bec89f9bd3
The-Gentleman <zraja4402@gmail.com>
2021-10-06 02:17:06 -0400

    added line breaks

--- pocket-doc-react-frontend/src/components/PatientList.js
+++ pocket-doc-react-frontend/src/components/PatientList.js
@@ -4,19 +4,60 @@ import { connect } from 'react-redux'
 import { fetchPatients  } from '../actions/patients'
 import { Link } from 'react-router-dom'
 
+
 class PatientList extends Component {
+    state = {
+        patients: this.props.patients,
+        query: ''
+    }
 
     componentDidMount() {
         this.props.dispatchFetchPatients()
     }
 
+    componentDidUpdate(prevProps){
+        if (this.props.patients !== prevProps.patients) {
+            this.setState({
+                patients: this.props.patients
+            })
+        }
+    }
+
+    handleClick = (event) => {
+        event.preventDefault();
+        let patientsObj = this.props.patients 
+        // patientsObj.sort((a,b) => a.name.localeCompare(b.name))
+    }
+
+    updateSearch = () => {
+        this.setState({
+            patients: this.props.patients.filter((patient) => patient.name.includes(this.state.query))
+        })
+    }
+
+    handleChange = (event) => {
+        event.preventDefault()
+        this.setState({
+            query: event.target.value
+        }, this.updateSearch)
+    }
+
     render() {
         return (
             <div className="patient-names">
+
+                <form>
+                    Search:
+                    <input type="text" value={this.state.query}onChange={this.handleChange}></input>
+                </form>
+
+
                 <h3>Your patients:</h3>
-                {this.props.patients.map(patient => 
-                    {return <ol><Link to={`patients/${patient.id}`}>{patient.name}</Link><LikeButton /></ol>})
+                <ol>
+                {this.state.patients.map(patient => 
+                    {return <Link key={patient.id} to={`patients/${patient.id}`}>{patient.name}<br></br></Link>})
                 }
+                </ol>
             </div>
         )
     }
@@ -34,3 +75,8 @@ const mapDispatchToProps = dispatch => {
         }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(PatientList);
+
+
+
+
+
